f37d1edbd49b3a6a59a3755ab9fdd91498e9fc71
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-18 18:43:48 +0530

    committed 10/18/2023, 6:43:47 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -13,17 +13,17 @@ int check_scalene(int a, int b, int c)
     int isscalene;
     if(a!=b)
     {
-        isscalene=0;
+       return isscalene=0;
     }
     else if(b!=c){
-        isscalene=0;
+        return isscalene=0;
     }
     else if(c!=0)
     {
     isscalene=0;
     }
     else{
-        isscalene!=0;
+        return isscalene!=0;
     }
     return isscalene;
 }
