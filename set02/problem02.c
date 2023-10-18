55ca175573baf56501cb7a7853a7e63dd288dd25
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-18 18:44:18 +0530

    committed 10/18/2023, 6:44:17 PM

--- set02/problem02.c
+++ set02/problem02.c
@@ -35,7 +35,7 @@ void output(int a, int b, int c, int isscalene)
     printf("The triangle with side %d %d %dis a scalene triangle ",a,b,c);
     
     }
-    else if(isscalene!=0)
+    else 
     {
 
         printf("It is not a scalene triangle");
