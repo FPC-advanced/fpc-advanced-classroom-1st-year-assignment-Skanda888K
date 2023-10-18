674f82532a080ff7791451086fd8fa2f9802ff05
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-18 18:33:47 +0530

    committed 10/18/2023, 6:33:46 PM

---
+++ set02/problem02.c
@@ -0,0 +1,43 @@
+//Write a program to find if a triangle is scalene.
+int input_side()
+{
+    int a;
+    printf("Enter the side of the traingle");
+    scanf("%d",&a);
+    return a;
+
+}
+int check_scalene(int a, int b, int c)
+{
+    int isscalene;
+    if(a!=b!=c)
+    {
+        isscalene=0;
+    }
+    else{
+        isscalene=1;
+    }
+    return isscalene;
+}
+void output(int a, int b, int c, int isscalene)
+{
+    if(isscalene=0){
+
+    
+    printf("The triangle with side %d %d %dis a scalene triangle ",a,b,c);
+    
+    }
+    else{
+        printf("Itis not a scalene triangle");
+    }
+}
+int main()
+{
+    int a,b,c,isscalene;
+a=input_side();
+b=input_side();
+c=input_side();
+isscalene=check_scalene(a,b,c);
+output(a,b,c,isscalene);
+return 0;
+}
