7fd33170c0692849400ef5249e07c90df0b4ed09
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-19 15:57:55 +0530

    committed 10/19/2023, 3:57:53 PM

--- set01/problem12.c
+++ set01/problem12.c
@@ -51,12 +51,12 @@ void output(int n, Complex c[n], Complex result)
 {
    printf("Sum of %d complex numbers:\n", n);
     for (int i = 0; i < n; i++) {
-        printf("%.2f + %.2fi ", c[i].real, c[i].imaginary);
+        printf("%f + %fi ", c[i].real, c[i].imaginary);
         if (i < n - 1) {
             printf("+ ");
         }
     }
-    printf("= %.2f + %.2fi\n", result.real, result.imaginary);
+    printf("= %f + %fi\n", result.real, result.imaginary);
 }
 
 int main()
