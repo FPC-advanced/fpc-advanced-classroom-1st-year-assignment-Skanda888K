86307cf94274146b9d3ea3cea7dff92bf2038e0c
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-25 09:57:47 +0530

    committed 10/25/2023, 9:57:47 AM

---
+++ set02/problem07.c
@@ -0,0 +1,34 @@
+//Write a program to find the area of a triangle
+
+
+typedef struct _triangle {
+	float base, altitude, area;
+} Triangle;
+
+Triangle input_triangle()
+{
+    Triangle t;
+    printf("Enter the base of triangle");
+    scanf("%f",&t.base);
+    printf("Enter the altitude of triangle");
+    scanf("%f",&t.altitude);
+    return t;
+}
+void find_area(Triangle *t)
+{
+    Triangle a,b;
+    t->area=0.5*(t->base*t->altitude);
+}
+void output(Triangle t)
+{
+    printf("The area triangle is %f",t.area);
+}
+int main()
+{
+    Triangle t;
+    t=input_triangle();
+
+    find_area(&t);
+    output(t);
+    return 0;
+}
