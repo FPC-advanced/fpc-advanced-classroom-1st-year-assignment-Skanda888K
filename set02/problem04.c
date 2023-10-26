1044a0fb2df0b0bf06285387245d57b0b271977d
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-26 15:49:16 +0530

    committed 10/26/2023, 3:49:15 PM

--- set02/problem04.c
+++ set02/problem04.c
@@ -2,5 +2,21 @@
 #include<stdio.h>
 int input_array_size()
 {
+    int n;
+    printf(" Enter the size of the array");
+    scanf("%d",&n);
+    return 0;
+
+}
+void input_array(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the array elements");
+        scanf("%d",a[i]);
+    }
+}
+int sum_composite_numbers(int n, int a[n])
+{
     
 }
