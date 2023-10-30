6780d57372375159d07185145eb1880dde6e3f0a
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-30 10:23:45 +0530

    committed 10/30/2023, 10:23:44 AM

--- set02/problem04.c
+++ set02/problem04.c
@@ -1,56 +1,57 @@
 //04.  Write a program to find Sum of composite number in an array of different numbers entered by the user.
-
-
-#include <stdio.h>
-
-int input_array_size() {
-    int n;
-    printf("Enter the size of the array: ");
-    scanf("%d", &n);
-    return n;
+#include<stdio.h>
+int input_array_size()
+{
+int n;
+printf("Enter the range of the array");
+scanf("%d",&n);
+return n;
 }
-
-void input_array(int n, int a[]) {
+void input_array(int n, int a[]) 
+{
     int i;
-    for (i = 0; i < n; i++) {
-        printf("Enter the elements of the array: ");
-        scanf("%d", &a[i]);
+    for(i=0;i<n;i++)
+    {
+        printf("Enter the array elements");
+        scanf("%d",&a[i]);
+
     }
 }
-
-int is_composite(int num) {
-    if (num <= 1) {
-        return 0; 
+int is_composite(int b)
+{
+    if(b<=1)
+    {        
+        return 0;
     }
-    for (int i = 2; i < num; i++) {
-        if (num % i == 0) {
-            return 1; 
-        }
-    }
-    return 0; 
+   for(int i=2;i<b;i++)
+   {
+    if(b%i==0);
+    return 1;
+   }
 }
-
-int sum_composite_numbers(int n, int a[]) {
-    int sum = 0;
-    for (int i = 0; i < n; i++) {
-        if (is_composite(a[i])) {
-            sum =sum + a[i];
-        }
+int sum_composite_numbers(int n, int a[n])
+{
+int sum=0;
+for(int i=0;i<n;i++)
+{
+    if( is_composite(a[i]))
+    {
+        sum=sum+a[i];
     }
     return sum;
 }
-
-void output(int sum) {
-    printf("The sum of composite numbers is: %d\n", sum);
+ 
 }
-
+void output(int sum)
+{
+    printf("The sum of all composite numbers are %d",sum); 
+}
 int main()
- {
-    int n, sum;
-    n = input_array_size();
-    int a[n];
-    input_array(n, a);
-    sum = sum_composite_numbers(n, a);
-    output(sum);
-    return 0;
+{
+    int sum,n,a[n];
+    n=input_array_size();
+    input_array(n,a);
+    sum=sum_composite_numbers(n,a);
+output(sum);
+
 }
