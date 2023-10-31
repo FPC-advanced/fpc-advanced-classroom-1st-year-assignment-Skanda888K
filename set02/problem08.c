4640d70d61650ce91d36480d092a6c5bd9906a9e
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-10-31 11:56:40 +0530

    committed 10/31/2023, 11:56:39 AM

---
+++ set02/problem08.c
@@ -0,0 +1,28 @@
+//Write a program to find the triangle with smallest area in n given triangles.
+typedef struct _triangle {
+	float base, altitude, area;
+} Triangle;
+int input_n()
+{
+    int n;
+    printf("Enter the bumber of triangle");
+    scanf("%d",&n);
+    return n;
+}
+Triangle input_triangle()
+{
+    Triangle x;
+    printf("Enter the base part");
+    scanf("%f",&x.area);
+    printf("Enter the altitude part");
+    scanf("%f",&x.altitude);
+    return x;
+}
+void input_n_triangles(int n, Triangle t[n])
+{
+    int i;
+    for(i=0;i<n;i++){
+        printf("ENter the array elements");
+        scanf("%d",&t[n]);
+    }
+}
