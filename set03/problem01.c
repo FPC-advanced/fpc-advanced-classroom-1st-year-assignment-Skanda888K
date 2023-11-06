6713602d6a65ea9deccd676056e82b730725957a
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-11-06 10:01:39 +0530

    committed 11/6/2023, 10:01:37 AM

--- set03/problem01.c
+++ set03/problem01.c
@@ -11,5 +11,16 @@ void input(float *x1, float *y1, float *x2, float *y2)
 float find_distance(float x1, float y1, float x2, float y2)
 {
     float dist=0;
-    dist=sqrt((x2-x1)+(y2-y1))
+    dist=sqrt(pow((x2-x1),2)+pow((y2-y1),2));
+    return dist;
+}
+void output(float x1, float y1, float x2, float y2, float distance)
+{
+    printf("The distance between the points(%f,%f) and (%f,%f)is %f",x1,y1,x2,y2,distance);
+
+}
+int main()
+{
+    float x1,x2,y1,y2,distance;
+    input(&x1,&y1,&x2,&y2);
 }
