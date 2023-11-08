4fabde5b3d9dba6a96f8d53dbb155e2f8f384987
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-11-08 19:30:46 +0530

    committed 11/8/2023, 7:30:45 PM

---
+++ set03/problem05.c
@@ -0,0 +1,45 @@
+// Write a program to find all the prime numbers between 2 to n Eratosthenes Sieve method.
+#include<stdio.h>
+
+int input_array_size()
+{
+    int n;
+    prontf("Enter the range of array");
+    scanf("%d",&n);
+    return n;
+}
+
+void init_array(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        a[i]=i;
+    }
+    
+}
+void erotosthenes_sieve(int n, int a[n])
+{
+  for(int i=2;i<n;i++)
+  {
+    if((i%2!=0)&&(i%3!=0)&&(i%5!=0)&&(i%7!=0))
+    {
+        i=a[n];
+    }
+  }
+}
+void output(int n, int a[n])
+{
+    for(int i=0;i<n;i++)
+    {
+        printf("The number of prime numbers between 2 to %d is %d",n,a[n]);
+    }
+}
+int main()
+{
+    int n,a[n];
+    n=input_array_size();
+     init_array( n,a);
+ output(n, a);
+ return 0;
+
+}
