feb6c391b76ce8efe390e2392ab1d016846ce36f
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-11-09 09:33:27 +0530

    committed 11/9/2023, 9:33:26 AM

--- set03/problem05.c
+++ set03/problem05.c
@@ -1,6 +1,6 @@
 // Write a program to find all the prime numbers between 2 to n Eratosthenes Sieve method.
 #include<stdio.h>
-
+#include<math.h>
 int input_array_size()
 {
     int n;
@@ -13,18 +13,18 @@ void init_array(int n, int a[n])
 {
     for(int i=0;i<n;i++)
     {
-       i=a[i];
+      i=a[i];
     }
     
 }
 void erotosthenes_sieve(int n, int a[n])
 {
-  for(int i=2;i<n;i++)
+  for(int i=2;i<=sqrt(n);i++)
   {
-    if((a[i]%2!=0)&&(a[i]%3!=0)&&(a[i]%5!=0)&&(a[i]%7!=0))
-    
-       a[n]=a[i];
-    
+   if(n%a[i]!=0)
+    {
+      n=a[n];
+    }
   }
 }
 void output(int n, int a[n])
