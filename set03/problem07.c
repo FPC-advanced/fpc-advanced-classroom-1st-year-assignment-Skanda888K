8b1ea473a930668a94008546f41473a92c9cfab1
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-11-20 09:42:57 +0530

    committed 11/20/2023, 9:42:56 AM

---
+++ set03/problem07.c
@@ -0,0 +1,14 @@
+//Write a program to find the length of a line
+typedef struct point {
+    float x, y;
+} Point;
+
+typedef struct line {
+    Point p1, p2;
+    float distance;
+} Line;
+Point input_point()
+{
+   
+    printf()
+}
