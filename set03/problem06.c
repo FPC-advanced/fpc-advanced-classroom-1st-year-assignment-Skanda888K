dd8d43b74542a925eefa98f1b4cd03588f2507d3
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-12-05 11:02:13 +0530

    committed 12/5/2023, 11:02:12 AM

--- set03/problem06.c
+++ set03/problem06.c
@@ -3,4 +3,16 @@
  {
     printf("Enter the string");
     scanf("%s",str);
+     printf("Enter the substring");
+    scanf("%s",substr);
+
  }
+ int sub_str_index(char* string, char* substr)
+ {
+   int i=0,index=-1,X;
+   for(int i=0;substr[i]="\0";i++)
+   {
+    X=i-1;  
+    i=0;
+   }
+   }
