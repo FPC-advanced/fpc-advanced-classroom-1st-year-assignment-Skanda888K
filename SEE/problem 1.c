4ac96b6023333582d9673f186b5d95793837ad47
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-12-19 14:10:24 +0530

    committed 12/19/2023, 2:10:23 PM

--- SEE/problem 1.c
+++ SEE/problem 1.c
@@ -1,1 +1,42 @@
 #include<stdio.h>
+int input()
+ { 
+     int n;
+     printf("Enter the number");
+     scanf("%d",&n);
+     return n;
+}
+
+ int find_fact(int n)
+{
+int result, a=1;
+
+for(int i=1;i<=n;i++)
+a*=i;
+if(a==n)
+{
+  result=1;
+}
+else{
+  result=0;
+}
+return result;
+}
+void output(int n, int result)
+{
+  if(result=1)
+{
+	printf("The number is  factorial");
+}
+   else{
+	printf("The number is not a factorial");
+}
+}
+int main()
+{
+    int n,result;
+n=input();
+result=find_fact(n);
+output(n,result);
+return 0;
+}
