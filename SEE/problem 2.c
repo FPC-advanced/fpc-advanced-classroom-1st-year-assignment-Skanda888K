a219ca99a48fe1a7422f5d742a097e7d3a882941
Skanda888K <skandak.bsc23@rvu.edu.in>
2023-12-19 14:19:24 +0530

    committed 12/19/2023, 2:19:23 PM

--- SEE/problem 2.c
+++ SEE/problem 2.c
@@ -1,52 +1,56 @@
-#include<stdio.h>
+#include <stdio.h>
 
 int input();
-void input_array(int n,int a[n]);
-void check_fact(int n,int a[n],int b[n]);
-void output(int n,int b[n]);
-
+void input_array(int n, int a[n]);
+void check_fact(int n, int a[n], int b[n]);
+void output(int n, int b[n]);
 
 int input()
 {
     int n;
-    printf("Enter the array size");
-    scanf("%d",&n);
+    printf("Enter the array size: ");
+    scanf("%d", &n);
     return n;
 }
-void input_array(int n,int a[n])
+
+void input_array(int n, int a[n])
 {
-    for(int i=0;i<n;++i)
+    for (int i = 0; i < n; ++i)
     {
-        printf("Enter the array elements");
-        scanf("%d",&a[i]);
+        printf("Enter the array element: ");
+        scanf("%d", &a[i]);
     }
 }
-void check_fact(int n,int a[n],int b[n])
+
+void check_fact(int n, int a[n], int b[n])
 {
-       int i, j, fact;
+    int i, j, fact;
     for (i = 0; i < n; i++)
-     {
+    {
         fact = 1;
-        for (j = 1; j <= a[i]; j++) 
+        for (j = 1; j <= a[i]; j++)
         {
             fact *= j;
         }
         b[i] = fact;
     }
 }
-void output(int n,int b[n])
+
+void output(int n, int b[n])
 {
-    for(int i=0;i<n;i++)
+    for (int i = 0; i < n; i++)
     {
-        printf("The factorial numbers in %d is %d",n,b[n]);
+        printf("The factorial of %d is %d\n", i, b[i]);
     }
 }
+
 int main()
 {
-    int n,a[n],b[n];
-    n=input();
-    input_array(n,a);
-    check_fact(n,a,b);
-    output(n,b);
+    int n;
+    n = input();
+    int a[n], b[n];
+    input_array(n, a);
+    check_fact(n, a, b);
+    output(n, b);
     return 0;
 }
